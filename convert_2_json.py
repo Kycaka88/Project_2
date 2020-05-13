441a2b8ddfc12d64af82c2900efa38e6e4439a22
kycaka88 <only.kycakaz2@gmail.com>
2020-05-13 22:38:56 +0300

    templates

--- convert_2_json.py
+++ convert_2_json.py
@@ -13,12 +13,12 @@ days = {
   "sun" : "Воскресенье"
 }
 
-with open("goals.json", "w") as f:
+with open("goals.json", "w", encoding="utf-8") as f:
    json.dump(goals, f, indent=4, ensure_ascii=False)
 
-with open("teachers.json", "w") as f:
+with open("teachers.json", "w", encoding="utf-8") as f:
    json.dump(teachers, f, indent=4, ensure_ascii=False)
 
-with open("days.json", "w") as f:
+with open("days.json", "w", encoding="utf-8") as f:
    json.dump(days, f, indent=4, ensure_ascii=False)
 
